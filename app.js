a1557e5cd81fba2ed81978a73f514eaf4be145f5
DevLest <lesterbonbiono@gmail.com>
2022-09-03 12:48:24 +0800

    change port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const homeRoutes = require("./routes/home");
 
 const app = express();
-app.set("port", process.env.PORT || 4455);
+app.set("port", process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "views"));
 
