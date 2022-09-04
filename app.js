7014f9195db8b751b61ad13beaa8e807bf9ad899
DevLest <lesterbonbiono@gmail.com>
2022-09-04 23:07:54 +0800

    Login initial update

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const express = require("express");
-const expressLayouts = require("express-layouts");
+// const expressLayouts = require("express-layouts");
 const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const session = require("express-session");
+const flash = require("connect-flash");
+
 const homeRoutes = require("./routes/home");
+const db = require("./lib/db");
+var setUpPassport = require("./lib/setuppassword");
 
 const app = express();
+setUpPassport();
+
 app.set("port", process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set("views", path.join(__dirname, "views"));
 
-app.use(expressLayouts);
+// app.use(expressLayouts);
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(session({
+    secret:"secretNode",
+    resave:false,
+    saveUninitialized:false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(homeRoutes.routes);
